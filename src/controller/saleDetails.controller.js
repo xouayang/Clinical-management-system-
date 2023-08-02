0d8f965bebf44b0a3ac29181f40b48d2f79b077c
Xouayang <xouayang2000@gmail.com>
2023-08-03 02:06:00 +0700

    fix api  saleDetails

--- src/controller/saleDetails.controller.js
+++ src/controller/saleDetails.controller.js
@@ -70,7 +70,7 @@ exports.get_of_list_order_medicines = async (req, res) => {
     of.price,mdt.unit,of.created_at from offers of
     inner join medicines md on of.medicines_id = md.medicines_id 
     inner join medicinestypes mdt on md.medicines_type_id = mdt.id
-    where of.status = 1
+    where of.status = 2
    `;
     const data = await sequelize.query(sql, { type: QueryTypes.SELECT });
     if (data) {
@@ -82,3 +82,97 @@ exports.get_of_list_order_medicines = async (req, res) => {
     return res.status(500).json({ message: error.message });
   }
 };
+exports.get_of_list_order_medicines_status1 = async (req, res) => {
+  try {
+    let outsideData1;
+    let rows=[]
+    let sum = 0;
+    const sql = `
+    select DISTINCT of.offer_id,of.bill_number,of.treat_id,of.status,mdt.type_name,of.name,of.amount,
+    of.price,mdt.unit,of.created_at,ft.name as firstcheck_name from offers of
+    inner join medicines md on of.medicines_id = md.medicines_id 
+    inner join medicinestypes mdt on md.medicines_type_id = mdt.id
+    inner join bills bl on of.treat_id = bl.id
+    inner join firstchecks ft on bl.firstcheck_id = ft.id
+    where of.status = 1
+   `;
+    const data = await sequelize.query(sql, { type: QueryTypes.SELECT });
+    if (data) {
+      for (let i = 0; i < data.length; i++) {
+         const outsideData = {
+          offer_id :data[i].offer_id,
+          bill_number:data[i].treat_id,
+          firstcheck_name:data[i].firstcheck_name,
+          status:data[i].status,
+          created_at:data[i].created_at
+         }
+         const inside = {
+          offer_id :data[i].offer_id,
+          treat_id:data[i].treat_id,
+          bill_number:data[i].bill_number,
+          firstcheck_name:data[i].firstcheck_name,
+          type_name:data[i].type_name,
+          name:data[i].name,
+          amount:data[i].amount,
+          price:data[i].price,
+          unit:data[i].unit,
+          status:data[i].status,
+          total_price:data[i].amount*data[i].price,
+          created_at:data[i].created_at
+         }
+         const item = data[i];
+         const itemTotal = item.amount * item.price;
+         sum += itemTotal;
+         outsideData1 = outsideData
+         rows.push(inside)
+      }
+      return res.status(200).json({
+        rows:rows
+      });
+    } else {
+      return res.status(200).json(data);
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+// get by id 
+exports.get_of_list_by_id = async (req, res) => {
+  try {
+     const {id} = req.params;
+    const sql = `
+    select DISTINCT of.treat_id,of.bill_number,mdt.type_name,of.name,of.amount,
+    of.price,mdt.unit,of.created_at from offers of
+    inner join medicines md on of.medicines_id = md.medicines_id 
+    inner join medicinestypes mdt on md.medicines_type_id = mdt.id
+    inner join bills bl on of.treat_id = bl.id
+    inner join firstchecks ft on bl.firstcheck_id = ft.id
+    where of.treat_id = '${id}' and of.status = 1
+   `;
+    const data = await sequelize.query(sql, { type: QueryTypes.SELECT });
+    if (data) {
+      let total_price = 0;
+      let sum = 0;
+      let bill ;
+      data.forEach(item => {
+        total_price = item.price * item.amount 
+        sum += total_price,
+        bill = item.bill_number
+      });
+       return res.status(200).json({rows:data,bill:bill,total_price:sum});
+    } else {
+      return res.status(200).json(data);
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+}
+// update status order medicines 
+exports.updateStatus = async (req, res) => {
+  try {
+    const {id} = req.params
+    await Offer.update({status:2}, {where:{treat_id:id}})
+  } catch (error) {
+    return res.status(500).json({message:error.message})
+  }
+}
--- src/routes/saleDetails.routes.js
+++ src/routes/saleDetails.routes.js
@@ -4,5 +4,8 @@ module.exports = (app) => {
     app.post("/create-saleDetails",verifyToken,controller.createSaleDetails)
     app.get("/get-offerrId/:id",controller.getOffer)
     app.get("/get-offer-status",controller.get_of_list_order_medicines)
+    app.get("/get-offer-status1",controller.get_of_list_order_medicines_status1)
+    app.get("/get-offer-status/:id",controller.get_of_list_by_id)
+    app.put("/update-medicines-status/:id",controller.updateStatus)
 
 }
